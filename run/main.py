e53369f41b4c34aaad64e0234b28d34cf1b1d484
Saiful Azad <mr.saiful.azad@gmail.com>
2025-06-25 08:58:07 +0600

    Add some code

--- run/main.py
+++ run/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return {"c":"azadq"}
+    return {"c":"Hello my job"}
 
 if __name__ == '__main__':
     app.run()
