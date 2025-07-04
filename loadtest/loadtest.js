abf10a1366d1da0f577a359ac7e05dfc9760fde0
rabbi <rafiqul15-2546@diu.edu.bd>
2025-07-04 20:07:11 +0600

    add loadtest code

--- loadtest/loadtest.js
+++ loadtest/loadtest.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 150,          // 10 virtual users
+  vus: 130,          // 10 virtual users
   duration: '1m',  // run test for 30 seconds
 };
 
