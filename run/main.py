12058b6799e6f95879a488ca674a04c04c3828af
Saiful Azad <mr.saiful.azad@gmail.com>
2025-07-26 21:20:06 +0600

    aa

--- run/main.py
+++ run/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     print("Function call")
-    i =1/0
+    i =1/1
     return {"m":"Abdullah & Mamun vai"}
 
 if __name__ == '__main__':
