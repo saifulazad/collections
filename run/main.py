ee545845eb3c150bb2f4aaa0d4fbff51cab42de1
Saiful Azad <mr.saiful.azad@gmail.com>
2025-07-26 21:13:19 +0600

    Add modification on python file

--- run/main.py
+++ run/main.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return {"m":"Hello my job is running"}
+    print("Function call")
+    i =1/0
+    return {"m":"Abdullah & Mamun vai"}
 
 if __name__ == '__main__':
     app.run()
